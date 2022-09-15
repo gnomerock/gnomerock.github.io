76fcbfcb91ba3714f429059b216cceb425d166cf
Nuttakorn Buajarern <gnomerock@users.noreply.github.com>
2022-09-15 15:14:47 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="my-8 mx-auto container">
-    <h3 class="text-blue-500">Gnomerock Studio</h3>
+  <div class="container mx-auto md:">
+    <img src="https://play.tailwindcss.com/img/beams.jpg" alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
+    <div class="absolute inset-0 bg-[url(https:/play.tailwindcss.com//img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+    <div class="relative mx-auto gap-2 rounded border-solid border-gray-300 bg-gray-50 p-5 shadow-md md:grid md:grid-cols-6">Gnomerock Studio</div>
   </div>
 </template>
 
